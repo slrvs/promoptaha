daee63c3087000f96befe487e4c3ad4783d9a8e2
PromoPtaha <jchameleonl96@gmail.com>
2026-07-16 10:13:23 +0300

    Add launch check script

---
+++ scripts/addLaunchCheckScript.cjs
@@ -0,0 +1,14 @@
+﻿const fs = require("fs");
+const path = require("path");
+
+const packagePath = path.join(process.cwd(), "package.json");
+const packageJson = JSON.parse(fs.readFileSync(packagePath, "utf8"));
+
+packageJson.scripts = packageJson.scripts || {};
+
+packageJson.scripts["launch:check"] =
+  "npm run preflight && npm run build && npm run smoke:dev";
+
+fs.writeFileSync(packagePath, JSON.stringify(packageJson, null, 2) + "\n", "utf8");
+
+console.log("Added npm script: launch:check");
